3d0e926721899390610801ede49a4a56416790b9
Abdullah Muayad <abdullahmajeed512@gmail.com>
2023-10-07 23:04:03 +0300

    should be last one before css

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-var Value = [];
-var firstNum;
-var secondNum;
-var operation = "";
-var secondNumberTrigger = 0;
-var result = 0;
+let expression = [""];
+let display = document.getElementById("display");
 
-function update(value) {
-  display.value += value;
-  if (secondNumberTrigger === 0) {
-    Value.push(value);
-    firstNum = Number(Value.join(""));
-
-    return firstNum;
-  } else {
-    Value.push(value);
-    secondNum = Number(Value.join(""));
-    return secondNum;
-  }
+function getNumber(num) {
+  expression.push(num);
+  display.value = expression.join("");
 }
 
-function operator(optr) {
-  display.value += optr;
-  Value = [""];
-  operation = optr;
-  secondNumberTrigger = 1;
-  return operation;
+function getOperator(operator) {
+  expression.push(operator);
+  display.value = expression;
+}
+function calculateDisplay() {
+  let string = document.querySelector("#display").value;
+  let result = processEquation(string);
+  document.querySelector("#display").value = result;
+}
+function processEquation(expression) {  /// the expression inside the parameter is different from the expression above
+  var match = expression.match(/\*|\/|\+|\-|\^/gims) || [];
+  while (match.length > 0) {
+    expression = expression
+      .replace(/\((.*?)\)/gims, (_, s) => {
+        return processEquation(s);
+      })
+      .replace(/([0-9\.\-]+)\^([0-9\.\-]+)/gims, (_, n1, n2) => {
+        return Math.pow(n1, n2);
+      })
+      .replace(/([0-9\.\-]+)(\*|\/)([0-9\.\-]+)/gims, (_, n1, o, n2) => {
+        return o == "*" ? Number(n1) * Number(n2) : Number(n1) / Number(n2);
+      })
+      .replace(/([0-9\.\-]+)(\-|\+)([0-9\.\-]+)/gims, (_, n1, o, n2) => {
+        return o == "+" ? Number(n1) + Number(n2) : Number(n1) - Number(n2);
+      });
+    match = expression.match(/\*|\/|\+|\-/gims) || [];
+  }
+  return expression;
 }
 
-function calculate() {
-  if (operation === "+") {
-    result = firstNum + secondNum;
-    display.value = result;
-    //   } else if (operation === "/") {
-    //     result = firstNum / secondNum;
-    //     display.value = result;
-    //   } else if (operation === "-") {
-    //     result = firstNum - secondNum;
-    //     display.value = result;
-    //   }
-  }
+function clearDisplay() {
+  document.getElementById("display").value = "";
+  expression = [""];
 }
