13eb2955bd61407802edbe4de718ec9ce2f4242c
Abdullah Muayad <abdullahmajeed512@gmail.com>
2023-10-09 23:59:24 +0300

    using array to save the values and compare the precedence manually

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-let expression = [""];
+let expression = "";
 let display = document.getElementById("display");
 
 function getNumber(num) {
-  expression.push(num);
-  display.value = expression.join("");
+  expression += num;
+  display.value = expression;
 }
 
 function getOperator(operator) {
-  expression.push(operator);
+  expression += operator;
   display.value = expression;
 }
 function calculateDisplay() {
@@ -15,28 +15,79 @@ function calculateDisplay() {
   let result = processEquation(string);
   document.querySelector("#display").value = result;
 }
-function processEquation(expression) {  /// the expression inside the parameter is different from the expression above
-  var match = expression.match(/\*|\/|\+|\-|\^/gims) || [];
-  while (match.length > 0) {
-    expression = expression
-      .replace(/\((.*?)\)/gims, (_, s) => {
-        return processEquation(s);
-      })
-      .replace(/([0-9\.\-]+)\^([0-9\.\-]+)/gims, (_, n1, n2) => {
-        return Math.pow(n1, n2);
-      })
-      .replace(/([0-9\.\-]+)(\*|\/)([0-9\.\-]+)/gims, (_, n1, o, n2) => {
-        return o == "*" ? Number(n1) * Number(n2) : Number(n1) / Number(n2);
-      })
-      .replace(/([0-9\.\-]+)(\-|\+)([0-9\.\-]+)/gims, (_, n1, o, n2) => {
-        return o == "+" ? Number(n1) + Number(n2) : Number(n1) - Number(n2);
-      });
-    match = expression.match(/\*|\/|\+|\-/gims) || [];
+function processEquation(expression) {
+  const operators = [];
+  const operands = [];
+
+  const precedence = {
+    "+": 1,
+    "-": 1,
+    "*": 2,
+    "/": 2,
+  };
+
+  const tokens = expression.match(/\d+|[\+\-\*\/\(\)]/g);
+
+  for (let token of tokens) {
+    if (token.match(/\d+/)) {
+      operands.push(parseFloat(token));
+    } else if (token.match(/[\+\-\*\/]/)) {
+      while (
+        operators.length > 0 &&
+        precedence[operators[operators.length - 1]] >= precedence[token]
+      ) {
+        const operator = operators.pop();
+        const rightOperand = operands.pop();
+        const leftOperand = operands.pop();
+        operands.push(applyOperator(leftOperand, operator, rightOperand));
+      }
+      operators.push(token);
+    } else if (token === "(") {
+      operators.push(token);
+    } else if (token === ")") {
+      while (operators.length > 0 && operators[operators.length - 1] !== "(") {
+        const operator = operators.pop();
+        const rightOperand = operands.pop();
+        const leftOperand = operands.pop();
+        operands.push(applyOperator(leftOperand, operator, rightOperand));
+      }
+      operators.pop(); // Remove the opening parenthesis
+    }
   }
-  return expression;
+
+  while (operators.length > 0) {
+    const operator = operators.pop();
+    const rightOperand = operands.pop();
+    const leftOperand = operands.pop();
+    operands.push(applyOperator(leftOperand, operator, rightOperand));
+  }
+
+  if (operands.length !== 1 || operators.length !== 0) {
+    throw new Error("Invalid Expression");
+  }
+
+  return operands[0];
+}
+
+function applyOperator(leftOperand, operator, rightOperand) {
+  switch (operator) {
+    case "+":
+      return leftOperand + rightOperand;
+    case "-":
+      return leftOperand - rightOperand;
+    case "*":
+      return leftOperand * rightOperand;
+    case "/":
+      if (rightOperand === 0) {
+        throw new Error("Division by zero");
+      }
+      return leftOperand / rightOperand;
+    default:
+      throw new Error("Invalid Operator");
+  }
 }
 
 function clearDisplay() {
   document.getElementById("display").value = "";
-  expression = [""];
+  expression = "";
 }
